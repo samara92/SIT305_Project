743056460ec034cda664d199a7e2277d4c987e7d
samara92 <spriyana@deakin.edu.au>
2018-05-14 02:36:35 +1000

    Add particle events

--- Sonic game/Assets/Scripts/LevelManager.cs
+++ Sonic game/Assets/Scripts/LevelManager.cs
@@ -6,6 +6,13 @@ public class LevelManager : MonoBehaviour {
 
 	public GameObject currentCheckPoint;
 	public PlayerController player;
+
+	//Particals for demonstration 
+	public GameObject deathPartical;
+	public GameObject respawnPartical;
+
+	public float respawnDelay;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -18,7 +25,24 @@ public class LevelManager : MonoBehaviour {
 
 	public void RespawnPlayer(){
 	
+		StartCoroutine ("RespawnPlayerCorrutine");
+	}
+
+	public IEnumerator RespawnPlayerCorrutine(){
+		//disable respawnPartical
+		//enable and play death partical
+		respawnPartical.SetActive(false);
+		deathPartical.SetActive (true);
+		player.enabled = false;
+		player.GetComponent<Renderer> ().enabled = false;
 		Debug.Log ("Player respawn !!!! xxxx");
+		yield return new WaitForSeconds (respawnDelay);
 		player.transform.position = currentCheckPoint.transform.position;
+		player.enabled = true;
+		player.GetComponent<Renderer> ().enabled = true;
+		//disable death partical
+		//enable and play respawn partical
+		respawnPartical.SetActive(true);
+		deathPartical.SetActive (false);
 	}
 }

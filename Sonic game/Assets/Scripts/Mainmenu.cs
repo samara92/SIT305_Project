8d834615cab227bc2bdc10b523febcdbd2d9865f
samara92 <spriyana@deakin.edu.au>
2018-05-15 22:00:45 +1000

    Main menu

---
+++ Sonic game/Assets/Scripts/Mainmenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Mainmenu : MonoBehaviour {
+
+	public void NewGame(){
+		SceneManager.LoadScene ("Level1");
+	}
+
+	public void QuitGame(){
+	
+		Application.Quit ();
+	}
+}

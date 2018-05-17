d03722e5886415deda701b8424f7117b03650f7d
samara92 <spriyana@deakin.edu.au>
2018-05-17 15:22:04 +1000

    set game audio

---
+++ Sonic game/Assets/LevelAnimationStop.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelAnimationStop : MonoBehaviour {
+
+	public GameObject ImageHolder;
+
+	public void StopAnimation(){
+		ImageHolder.SetActive (false);
+	}
+}
